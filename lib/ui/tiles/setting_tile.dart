f84dd219730c7e4bbc0f18d6a78340227d8a34ee
Jesus Acosta <jesusgabriel.91@gmail.com>
2023-03-07 15:45:36 -0400

    Add new SettingTile

--- lib/ui/tiles/setting_tile.dart
+++ lib/ui/tiles/setting_tile.dart
@@ -2,6 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:songtube/ui/components/circular_check_box.dart';
 import 'package:songtube/ui/text_styles.dart';
 
+class SettingTile extends StatelessWidget {
+  const SettingTile({
+    required this.title,
+    required this.subtitle,
+    required this.leadingIcon,
+    required this.onTap,
+    this.enabled = true,
+    super.key});
+  final String title;
+  final String subtitle;
+  final IconData leadingIcon;
+  final Function() onTap;
+  final bool enabled;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      enabled: enabled,
+      onTap: () => onTap,
+      leading: SizedBox(
+        height: double.infinity,
+        child: Icon(leadingIcon, color: enabled ? Theme.of(context).primaryColor : Colors.grey.withOpacity(0.6)),
+      ),
+      title: Text(title, style: subtitleTextStyle(context, bold: true).copyWith(color: enabled ? null : Colors.grey.withOpacity(0.6))),
+      subtitle: Text(subtitle, style: tinyTextStyle(context, opacity: 0.7).copyWith(color: enabled ? null : Colors.grey.withOpacity(0.6))),
+    );
+  }
+}
+
 class SettingTileCheckbox extends StatelessWidget {
   const SettingTileCheckbox({
     required this.title,
