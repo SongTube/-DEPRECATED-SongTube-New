079539071b4fc58fc60065856421a1c660d4905c
Jesus Acosta <jesusgabriel.91@gmail.com>
2023-04-30 22:24:27 -0400

    Implement search on music page

--- lib/screens/home/home_music/home_music.dart
+++ lib/screens/home/home_music/home_music.dart
@@ -12,7 +12,9 @@ import 'package:songtube/screens/home/home_music/pages/artists_page.dart';
 import 'package:songtube/screens/home/home_music/pages/home_page.dart';
 import 'package:songtube/screens/home/home_music/pages/music_page.dart';
 import 'package:songtube/screens/home/home_music/pages/playlists_page.dart';
+import 'package:songtube/screens/home/home_music/pages/search_page.dart';
 import 'package:songtube/ui/animations/blue_page_route.dart';
+import 'package:songtube/ui/components/custom_inkwell.dart';
 import 'package:songtube/ui/rounded_tab_indicator.dart';
 import 'package:songtube/ui/text_styles.dart';
 
@@ -29,6 +31,11 @@ class _HomeMusicState extends State<HomeMusic> with TickerProviderStateMixin {
   late TabController tabController = TabController(length: 5, vsync: this, initialIndex: AppSettings.defaultLandingMusicPage);
 
   MediaItem? latestEvent;
+
+  // Search Query
+  TextEditingController searchController = TextEditingController();
+  FocusNode searchFocusNode = FocusNode();
+  bool get searching => searchController.text.trim().isNotEmpty;
 
   @override
   void initState() {
@@ -67,7 +74,10 @@ class _HomeMusicState extends State<HomeMusic> with TickerProviderStateMixin {
           SizedBox(
             height: kToolbarHeight-8,
             child: _appBar()),
-          _tabs(),
+          AnimatedSize(
+            duration: const Duration(milliseconds: 200),
+            child: searching ? Container(padding: const EdgeInsets.only(top: 12)) : _tabs(),
+          ),
           Divider(height: 1, color: Theme.of(context).dividerColor),
           Expanded(child: _body()),
         ],
@@ -76,38 +86,47 @@ class _HomeMusicState extends State<HomeMusic> with TickerProviderStateMixin {
   }
 
   Widget _searchBar() {
-    return InkWell(
-      onTap: () {
-        // UiUtils.pushRouteAsync(context, const SearchScreen());
-      },
-      child: Container(
-        margin: const EdgeInsets.only(left: 20, right: 20),
-        padding: const EdgeInsets.only(left: 16),
-        height: kToolbarHeight,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100),
-          color: Theme.of(context).textTheme.bodyText1!.color!.withOpacity(0.05),
-        ),
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: Row(
-            children: [
-              const Icon(Iconsax.search_normal, size: 18),
-              const SizedBox(width: 16),
-              Expanded(
-                child: TextField(
-                  enabled: false,
-                  style: subtitleTextStyle(context).copyWith(fontWeight: FontWeight.w500),
-                  decoration: InputDecoration.collapsed(
-                    hintStyle: smallTextStyle(context, opacity: 0.4).copyWith(fontWeight: FontWeight.w500),
-                    hintText: 'Search anything...'),
-                ),
-              ),
-              const SizedBox(width: 16),
-            ],
-          ),
-        )
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 20),
+      padding: const EdgeInsets.only(left: 16),
+      height: kToolbarHeight,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: Theme.of(context).textTheme.bodyText1!.color!.withOpacity(0.05),
       ),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Row(
+          children: [
+            const Icon(Iconsax.search_normal, size: 18),
+            const SizedBox(width: 16),
+            Expanded(
+              child: TextField(
+                enabled: true,
+                focusNode: searchFocusNode,
+                controller: searchController,
+                style: smallTextStyle(context).copyWith(fontWeight: FontWeight.w500),
+                decoration: InputDecoration.collapsed(
+                  hintStyle: smallTextStyle(context, opacity: 0.4).copyWith(fontWeight: FontWeight.w500),
+                  hintText: 'Search anything...'),
+                onChanged: (_) {
+                  setState(() {});
+                },
+              ),
+            ),
+            if (searchController.text.trim().isNotEmpty)
+            CustomInkWell(
+              onTap: () {
+                searchController.clear();
+                searchFocusNode.unfocus();
+                setState(() {});
+              },
+              child: Icon(Icons.clear, color: Theme.of(context).iconTheme.color, size: 18),
+            ),
+            const SizedBox(width: 16),
+          ],
+        ),
+      )
     );
   }
 
@@ -159,22 +178,41 @@ class _HomeMusicState extends State<HomeMusic> with TickerProviderStateMixin {
               Text('Please wait a moment...', style: subtitleTextStyle(context, opacity: 0.6))
             ],
           ))
-        : TabBarView(
-            physics: const BouncingScrollPhysics(),
-            controller: tabController,
+        : Stack(
             children: [
-              // Home
-              HomePage(onSwitchIndex: (index) => tabController.animateTo(3)),
-              // Music
-              const MusicPage(),
-              // Playlists
-              const PlaylistsPage(),
-              // Albums
-              const AlbumsPage(),
-              // Artists
-              const ArtistsPage(),
-            ]
-          ),
+              _tabBar(),
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 400),
+                reverseDuration: const Duration(milliseconds: 200),
+                switchInCurve: Curves.ease,
+                child: searching ? _searchBox() : const SizedBox(),
+              )
+            ],
+          )
     );
   }
+
+  Widget _tabBar() {
+    return TabBarView(
+      physics: const BouncingScrollPhysics(),
+      controller: tabController,
+      children: [
+        // Home
+        HomePage(onSwitchIndex: (index) => tabController.animateTo(3)),
+        // Music
+        const MusicPage(),
+        // Playlists
+        const PlaylistsPage(),
+        // Albums
+        const AlbumsPage(),
+        // Artists
+        const ArtistsPage(),
+      ]
+    );
+  }
+
+  Widget _searchBox() {
+    return MusicSearchPage(searchController: searchController);
+  }
+
 }
